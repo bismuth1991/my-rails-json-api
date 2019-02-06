a7a6173081e2772c0fba2ad6d601f6c49388db04
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-06 02:18:26 -0500

    Create services routes under dejavu_api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :dejavu_api, defaults: { format: :json } do
+    resources :services, except: [:put]
+  end
 end

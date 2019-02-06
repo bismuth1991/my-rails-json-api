a334297ca245bb9fd702ccc49770472efa9db191
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-06 02:14:57 -0500

    Write request specs, GET #index for ServicesController

---
+++ spec/requests/dejavu_api/services_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'DejaVu Services API', type: :request do
+  before(:each) do 
+    Service.create(name: "Manicure", price: 15)
+    Service.create(name: "Pedicure", price: 28)
+    Service.create(name: "Gel Manicure", price: 30)
+  end
+
+  describe "GET #index" do 
+    before { get "/dejavu_api/services/" }
+
+    it "responses with status code of 200" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "fectches all services" do 
+      num_services = JSON.parse(response.body).length
+      expect(num_services).to eq(3)
+    end
+
+    it "fetches each service in correct format" do 
+      JSON.parse(response.body).values.each do |service|
+        expect(service).to match_response_schema('dejavu_service')
+      end
+    end
+  end
+end

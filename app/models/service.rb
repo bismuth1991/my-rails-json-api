3b39c307111220ea6a053258f369f13611c81717
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-05 23:59:31 -0500

    Change let to before in Service model spec. Configure .rspec

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: services
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string           not null
+#  price      :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Service < ApplicationRecord
 end
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,8 +1,19 @@
+# == Schema Information
+#
+# Table name: services
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string           not null
+#  price      :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
   describe "name validations" do
-    let { Service.create(name: "Manicure", price: 15) }
+    before { Service.create(name: "Manicure", price: 15) }
 
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }

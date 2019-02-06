f1de04394086b03601ec3ac68f7ccb9d77bfc62a
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-05 23:52:04 -0500

    Create services table migration

--- db/migrate/20190206044142_create_services.rb
+++ db/migrate/20190206044142_create_services.rb
@@ -1,7 +1,9 @@
 class CreateServices < ActiveRecord::Migration[5.2]
   def change
     create_table :services do |t|
-
+      t.string :name, null: false, unique: true
+      t.integer :price, null: false
+      
       t.timestamps
     end
   end

67b351aec408720f37469a398fc01d1c0f2bc736
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-05 23:55:16 -0500

    Fix unique constrains in services table migration

--- db/migrate/20190206044142_create_services.rb
+++ db/migrate/20190206044142_create_services.rb
@@ -1,10 +1,12 @@
 class CreateServices < ActiveRecord::Migration[5.2]
   def change
     create_table :services do |t|
-      t.string :name, null: false, unique: true
+      t.string :name, null: false
       t.integer :price, null: false
       
       t.timestamps
     end
+
+    add_index :services, :name, unique: true
   end
 end

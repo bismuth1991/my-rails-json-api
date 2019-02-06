c443389f75b5f99a4c6901446396f87ae471308a
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-06 01:48:44 -0500

    run annotate gem

--- app/models/receipt_service.rb
+++ app/models/receipt_service.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: receipt_services
+#
+#  id         :bigint(8)        not null, primary key
+#  receipt_id :integer          not null
+#  service_id :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class ReceiptService < ApplicationRecord
   belongs_to :service 
   belongs_to :receipt
--- spec/models/receipt_service_spec.rb
+++ spec/models/receipt_service_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: receipt_services
+#
+#  id         :bigint(8)        not null, primary key
+#  receipt_id :integer          not null
+#  service_id :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe ReceiptService, type: :model do

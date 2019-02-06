29ec6c3f1b5e6c4bfcf236364000ee17cebbb14e
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-06 02:29:02 -0500

    Configure Jbuilder camilize keys format in config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Camelize keys in jbuilder view for frontend
+Jbuilder.key_format camelize: :lower
